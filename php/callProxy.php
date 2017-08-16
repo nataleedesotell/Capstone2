013a741275de60224b86e3c9bbd22258c6f600c8
nataleedesotell <nataleerobson@gmail.com>
2017-08-16 11:31:20 -0400

    minor style edits, add to do list\

--- php/callProxy.php
+++ php/callProxy.php
@@ -1,5 +1,5 @@
 <?php
-include 'C:\xampp\htdocs\php\cartoDBProxy.php';
+include 'C:\Users\natal\Desktop\CentralPark777\Capstone2\php\cartoDBProxy.php';
 //			^CHANGE THIS TO THE PATH TO YOUR cartodbProxy.php
 $queryURL = $_POST['qurl'];
 $return = goProxy($queryURL);
